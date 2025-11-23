e23fd25390d74686f569e952bb6cc76910157ce1
olyasha4U <lybo.km15@gmail.com>
2025-11-23 22:04:50 +0200

    commit11

--- main.py
+++ main.py
@@ -11,22 +11,33 @@ import requests, sqlite3
 # print(a)
 # connection.close()
 from bs4 import BeautifulSoup
-# import requests
-# response = requests.get("https://
-# coinmarketcap.com/")
-# if response.status_code == 200:
-#   soup = BeautifulSoup(response.text,
-#   features="html.parser")
-#   soup_list = soup.find_all("a", {"href": "/
-#   currencies/bitcoin/markets/"})
-# res = soup_list[0].find("span")
-# print(res.text)
+
 # 2
 
+# response=requests.get("https://books.toscrape.com/")
+# if response.status_code==200:
+#     soup =BeautifulSoup(response.text,features="html.parser")
+#     prices = soup.find_all("p", class_="price_color")
+#
+#     for price in prices:
+#         print(price.text)
+# 3
 response=requests.get("https://books.toscrape.com/")
-if response.status_code==200:
-    soup =BeautifulSoup(response.text,features="html.parser")
-    prices = soup.find_all("p", class_="price_color")
+if response.status_code== 200:
+    soup=BeautifulSoup(response.text, "html.parser")
+    title = soup.find_all("h3")
+    prices= soup.find_all("p", class_="price_color")
+    availability=soup.find_all("p", class_="instock availability")
+    for a in availability:
+        availability_text=a.text.strip()
+        print(availability_text)
 
-    for price in prices:
-        print(price.text)
+    for t in title:
+        title_text = t.find("a")["title"]
+
+        print(title_text)
+    for p in prices:
+        price_text = p.text.strip()
+
+        print(price_text)
+
